6a4cd75f9e8e2ee8892816401b606dd0933d8a56
Justin Dearing <jdearing@whikerlabs.com>
2018-12-23 14:21:03 -0500

    removed a couple required params

--- RenamePhoto.ps1
+++ RenamePhoto.ps1
@@ -4,8 +4,6 @@
 
 function rename-photo(
     [ValidateScript({Test-Path $_})][string]$fileName,
-	[String]$device,
-	[String]$location,
     [int]$AddHours){
 
 	if ($fileName -eq "") {
@@ -20,25 +18,15 @@ function rename-photo(
 		return;
 	}
 
-	if ($device -eq "") {
-		write-host "Please provide a device name, e.g. iPhoneLB!" -foregroundcolor "red"
-		write-host 'Full example: dir *.jpg | % { Rename-Photo $_.FullName "iPhoneLB" "Martinique" }'
-		return;
-	}
-
-	if ($location -eq "") {
-		write-host "Please provide a location, e.g. Martinique!" -foregroundcolor "red"
-		write-host 'Full example: dir *.jpg | % { Rename-Photo $_.FullName "iPhoneLB" "Martinique" }'
-		return;
-	}
-
 	$null = [reflection.assembly]::LoadWithPartialName("System.Drawing")
 	$pic = New-Object System.Drawing.Bitmap($fileName)
 
-	# via http://stackoverflow.com/questions/6834259/how-can-i-get-programmatic-access-to-the-date-taken-field-of-an-image-or-video
-	$bitearr = $pic.GetPropertyItem(36867).Value # Date Taken
+	if ($pic) {
+		# via http://stackoverflow.com/questions/6834259/how-can-i-get-programmatic-access-to-the-date-taken-field-of-an-image-or-video
+		$bitearr = $pic.GetPropertyItem(36867).Value # Date Taken
 
-	$pic.Dispose()
+		$pic.Dispose()
+	}
 
 	if ($bitearr -ne $null) {
 
@@ -48,25 +36,17 @@ function rename-photo(
     } else {
 
 		# we could not extract an EXIF "Date Taken".
-		# perhaps we can one of these dates instead.
-		# CreationTime              Property       datetime CreationTime {get;set;}
-		# CreationTimeUtc           Property       datetime CreationTimeUtc {get;set;}
-		# LastAccessTime            Property       datetime LastAccessTime {get;set;}
-		# LastAccessTimeUtc         Property       datetime LastAccessTimeUtc {get;set;}
-		# LastWriteTime             Property       datetime LastWriteTime {get;set;}
-		# LastWriteTimeUtc          Property       datetime LastWriteTimeUtc {get;set;}
-		dir $fileName | % { $exactDate = $_.LastWriteTime; }
+		Get-ChildItem $fileName | ForEach-Object { $exactDate = $_.CreationTime; }
 	}
 
 	if ($AddHours -ne 0) {
 		$exactDate = $exactDate.AddHours($AddHours)
 	}
 
-    $length = (dir $fileName | % length )
+    $length = (Get-ChildItem $fileName | ForEach-Object length )
 	$extensionWithDot = [io.path]::GetExtension($FileName)
-	$newName = ("{0:yyyy-MM-dd-HH-mm-ss}_{1}_{2}_{3}{4}" -f $exactDate, $device, $location, $length, $extensionWithDot)
+	$newName = ("{0:yyyy-MM-dd-HH-mm-ss}-{1}{2}" -f $exactDate, $length, $extensionWithDot)
 
 	write-host "Creating: $newName"
 	rename-item $fileName $newName
 }
-
